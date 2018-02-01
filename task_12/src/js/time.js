78e49e81abf1c2c55a007dc5735d215e8a0ffef9
Tania Maslivets <t.mykhailiuk1@gmail.com>
2018-02-01 12:02:58 +0200

    Add style and adaptive (without tablet)

---
+++ task_12/src/js/time.js
@@ -0,0 +1,36 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+    var clock = document.getElementsByClassName("time")[0];
+
+    console.log(clock);
+    
+    function realTimeclock() {
+        var time = new Date();
+        var hours = time.getHours().toString();
+        var min = time.getMinutes().toString();
+        var sec = time.getSeconds().toString();
+
+        if (hours.length < 2) {
+            hours = '0' + hours;
+        }
+        if (min.length < 2) {
+            min = '0' + min;
+        }
+
+        if (sec.length < 2) {
+            sec = '0' + sec;
+        }
+
+        var clockString = hours + ' : ' + min + ' : ' + sec;
+
+        clock.textContent = clockString;
+
+    }
+
+
+    setInterval(realTimeclock, 1000);
+
+});
+
+
